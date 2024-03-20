c2304ed563ba17c0d296e46145a923e076d351f7
Patrick Ober <patrick.ober@world-direct.at>
2024-03-20 13:29:03 +0100

    add function to function auth and remove secret

--- func-secapp/GetHello.cs
+++ func-secapp/GetHello.cs
@@ -1,7 +1,10 @@
+using System.Net.Http.Headers;
+using Azure.Core;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Azure.Identity;
 
 namespace func_secapp
 {
@@ -15,10 +18,31 @@ namespace func_secapp
         }
 
         [Function("GetHello")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
-        {
-            _logger.LogInformation("C# HTTP trigger function: 'sec-app' processed a request.");
-            return new OkObjectResult("Hello from func-secapp");
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req) {
+
+            var clientId = "3d14b644-fee0-4f1f-af2e-0343c86148c0";
+            var clientSecret = Environment.GetEnvironmentVariable("ClientSecret");
+            var tenantId = "3ea78af3-094b-492f-9237-8077bc4f31ab";
+
+            var functionUrl = "https://apim-secapp-westeurope-001.azure-api.net/pobfuncopen/GetHello";
+            var scope = "api://3d14b644-fee0-4f1f-af2e-0343c86148c0/.default";
+
+            var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
+            var tokenRequestContext = new TokenRequestContext(new[] { scope });
+            var accessToken = await credential.GetTokenAsync(tokenRequestContext);
+
+            using var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.Token);
+
+            var response = await httpClient.GetAsync(functionUrl);
+
+            if (!response.IsSuccessStatusCode) {
+                _logger.LogInformation("HTTP call to Azure Function through API Management failed!");
+                return new OkObjectResult("boo!");
+            }
+            _logger.LogInformation("HTTP call to Azure Function through API Management succeeded!");
+            return new OkObjectResult("yay!");
         }
     }
 }
